f8973204815fe8b98fa1a1a3e8b35e2cda978493
Dharmik Shah <dharmik.shah@envision-energy.com>
2017-08-05 12:32:54 -0500

    Code cleanup, created component using function and class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const Card1 = () => {
+    return (
+        <div>
+            <h1>Card 1</h1>
+        </div>
+    )
+};
+
+const Card2 = () => {
+    return (
+        <div>
+            <h1>Card 2</h1>
+        </div>
+    )
+};
+
+class Card3 extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Card 3</h1>
+            </div>
+        )
+    }
+}
+
+//top level class component which contains child components to manage the state object and event handler
+class CardList extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello React!!</h1>
+                <Card1/>
+                <Card2/>
+                <Card3/>
+            </div>
+        )
+    }
+}
+;
+
+ReactDOM.render(<CardList/>, document.getElementById('root'));
 registerServiceWorker();
